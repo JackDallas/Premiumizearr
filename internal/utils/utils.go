14c82184d2c28378e1f1286ead2ab5857a96b59e
Jack Dallas <3620144+JackDallas@users.noreply.github.com>
2022-06-04 21:32:46 +0100

    Pre-set all directory locations for Docker

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -118,3 +118,16 @@ func EnvOrDefault(envName string, defaultValue string) string {
 	}
 	return envValue
 }
+
+func IsRunningInDockerContainer() bool {
+	// docker creates a .dockerenv file at the root
+	// of the directory tree inside the container.
+	// if this file exists then the viewer is running
+	// from inside a container so return true
+
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		return true
+	}
+
+	return false
+}
