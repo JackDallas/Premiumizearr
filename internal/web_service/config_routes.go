91cf5bcfc83e3ca7f536a71a9640e5e07ce2bb7f
Jack Dallas <3620144+JackDallas@users.noreply.github.com>
2022-06-04 03:50:46 +0100

    API: Config Service Config: Refactor Implementation

---
+++ internal/web_service/config_routes.go
@@ -0,0 +1,57 @@
+package web_service
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/jackdallas/premiumizearr/internal/config"
+)
+
+type ConfigChangeResponse struct {
+	Succeeded bool   `json:"succeeded"`
+	Status    string `json:"status"`
+}
+
+func (s *server) ConfigHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodGet:
+		data, err := json.Marshal(s.config)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(data)
+	case http.MethodPost:
+		var newConfig config.Config
+		err := json.NewDecoder(r.Body).Decode(&newConfig)
+		if err != nil {
+			EncodeAndWriteConfigChangeResponse(w, &ConfigChangeResponse{
+				Succeeded: false,
+				Status:    fmt.Sprintf("Config failed to update %s", err.Error()),
+			})
+			return
+		}
+		s.config = &newConfig
+		s.config.Save()
+		EncodeAndWriteConfigChangeResponse(w, &ConfigChangeResponse{
+			Succeeded: true,
+			Status:    "Config updated",
+		})
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func EncodeAndWriteConfigChangeResponse(w http.ResponseWriter, resp *ConfigChangeResponse) {
+	data, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(data)
+}
