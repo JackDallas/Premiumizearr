5a860df57fe1e244466ee957e2ecff11e95797c6
Jack Dallas <3620144+JackDallas@users.noreply.github.com>
2022-06-26 21:15:22 +0000

    Don't full remove unzip dir on startup

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -151,3 +151,23 @@ func IsDirectoryWriteable(path string) bool {
 
 	return true
 }
+
+//https://stackoverflow.com/questions/33450980/how-to-remove-all-contents-of-a-directory-using-golang
+func RemoveContents(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+			return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+			return err
+	}
+	for _, name := range names {
+			err = os.RemoveAll(filepath.Join(dir, name))
+			if err != nil {
+					return err
+			}
+	}
+	return nil
+}
